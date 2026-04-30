348ee7c1c1f604e12f802c9c4ccf018b9fd91d93
mojtaba.fallah <mojtaba.fallah1373@gmail.com>
2026-04-30 22:06:05 +0330

    first commit

---
+++ zarsam_odoo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Zarsam Odoo
+ * Plugin URI:  https://zarsam.com/
+ * Description: ب
+ * Version:     1.0.0
+ * Author:      Mojtaba Fallah
+ * Author URI:  https://github.com/mojtabafallah/
+ */
